f819cbe15ef6cdd4c994d5bb9b70184d37fcee71
hfcorriez <hfcorriez@gmail.com>
2013-09-10 22:32:15 +0800

    Add more comments

--- lib/Qiniu/Mac.php
+++ lib/Qiniu/Mac.php
@@ -13,18 +13,37 @@ class Mac
         $this->secret_key = $secret_key;
     }
 
+    /**
+     * Sign data
+     *
+     * @param string $data
+     * @return string
+     */
     public function sign($data)
     {
         $sign = hash_hmac('sha1', $data, $this->secret_key, true);
         return $this->access_key . ':' . Util::uriEncode($sign);
     }
 
+    /**
+     * Sign with data
+     *
+     * @param string $data
+     * @return string
+     */
     public function signWithData($data)
     {
         $data = Util::uriEncode($data);
         return $this->sign($data) . ':' . $data;
     }
 
+    /**
+     * Sign url and body for generate token
+     *
+     * @param string $url
+     * @param string $body
+     * @return string
+     */
     public function signRequest($url, $body = '')
     {
         $url = parse_url($url);
